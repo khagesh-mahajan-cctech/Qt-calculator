9b25f75e3a4e9d5bc910f491295c45aa28d5d186
khagesh-mahajan-cctech <khagesh.mahajan@cctech.co.in>
2022-08-16 14:17:28 +0530

    Square root function completed

--- Qt-calculator/logic.cpp
+++ Qt-calculator/logic.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 double unaryOperation(std::string btn, std::string text)
 {
@@ -17,6 +18,11 @@ double unaryOperation(std::string btn, std::string text)
     else if(btn == "1/x")
     {
         number = 1 / number;
+        return number;
+    }
+    else if(btn == "√")
+    {
+        number = sqrt(number);
         return number;
     }
     else
